8733db541b062c7e7a4b918552a82d362ac25f0f
vladexnik <nikityuk_2015_smile@mail.ru>
2024-03-27 23:29:49 +0300

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-   env: {
-      browser: true,
-      es2021: true,
-   },
-   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-   parser: '@babel/eslint-parser',
-   overrides: [
-      {
-         env: {
-            node: true,
-         },
-         files: ['.eslintrc.{js,cjs}'],
-         parserOptions: {
-            sourceType: 'script',
-         },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parser: '@babel/eslint-parser',
+  overrides: [
+    {
+      env: {
+        node: true,
       },
-   ],
-   parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-   },
-   rules: {
-      'react/prop-types': 0,
-      'prettier/prettier': [
-         'warn',
-         {
-            endOfLine: 'auto',
-         },
-      ],
-   },
+      files: ['.eslintrc.{js,cjs}', 'src/*/.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/prop-types': 0,
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
